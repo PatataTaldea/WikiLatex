88614abc089010c1e316544bcbe4a4e04d02bf88
osainz59 <osainz59@gmail.com>
2017-11-15 10:22:42 +0100

    JQuery -> CDN

--- www/index.php
+++ www/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <!-- Javascript -->
-        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="js/script.js"></script>
 
         <!-- CSS -->
