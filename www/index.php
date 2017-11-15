5cc05ac7085623628ad47602506953c05653b997
osainz59 <osainz59@gmail.com>
2017-11-15 10:19:21 +0100

    Hasierako fitxategiak igotzen

---
+++ www/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE xhtml>
+<html>
+    <head>
+        <!-- Javascript -->
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/script.js"></script>
+
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+
+    </body>
+</html>
