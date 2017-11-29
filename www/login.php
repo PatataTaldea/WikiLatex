58a53c49b3ab5f4648e23a0c67b0ca4d59b6ecb5
Oscar Sainz Jimenez <osainz59@gmail.com>
2017-11-29 23:18:43 +0100

    Zuzenketa txikiak

--- www/login.php
+++ www/login.php
@@ -27,7 +27,6 @@
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<html>
     <head>
         <!-- Javascript -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
