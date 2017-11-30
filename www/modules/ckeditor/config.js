9d0a12b0d978d8dd4a1c2919cbee2359b730242d
Maite <urragorospe@hotmail.com>
2017-11-30 21:04:10 +0100

    Artikulu berria, editorea

---
+++ www/modules/ckeditor/config.js
@@ -0,0 +1,17 @@
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	config.extraPlugins = 'eqneditor'
+	config.resize_minWidth = 400;
+	config.resize_minHeight = 600;
+	config.resize_maxWidth = 700;
+	config.resize_maxHeight = 800;
+	config.height = 250;
+	//config.extraPlugins = 'uploadfile'
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+};
