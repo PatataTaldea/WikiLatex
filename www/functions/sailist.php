c3771940be8a9a6a6e9dd92c5b92fcc4059f53cf
Egonichan <egoninobi@gmail.com>
2017-12-07 19:23:34 +0100

    errorea kenduta Oskar maisuari esker

--- www/functions/sailist.php
+++ www/functions/sailist.php
@@ -4,34 +4,33 @@ $data = simplexml_load_file("../".ARTIKULUAK) or die("Error: Cannot create objec
 ?>
 
 <?php 
-  if (isset($_GET('saila')) && $_GET('saila')=='HOME') {
+  if (isset($_GET['saila']) && $_GET['saila']=='HOME') {
   ?>
     <h1>HOMEEEEEEEEEEEEEEEEE</h1>
 <?php 
-} else if (isset($_GET('saila')) && $_GET('saila')=='Kontaktua') {
+} else if (isset($_GET['saila']) && $_GET['saila']=='Kontaktua') {
   ?>
     <h1>KONTAKTUAAAAAAAAAAAA</h1>
 <?php 
 } else {
   ?>
     <h1>Ekuazioak eta ikurrak<button type="button" class="btn btn-outline-primary" onclick="sortuArtikulua()">Sortu artikulu berri bat</button></h1>
+
 <?php
-  }
-  
-foreach($data->artikuloa as $artikuloa){
+    foreach($data->artikuloa as $artikuloa){
   ?>
-<div class="list-group" onclick="console.log('patata');">
-  <a href="<?php echo $artikuloa->textua;?>" class="list-group-item list-group-item-action flex-column align-items-start">
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1"><?php echo $artikuloa->izenburua;?></h5>
-      <small><?php echo $artikuloa->saila; ?></small>
-    </div>
-    <p class="mb-1"><?php echo $artikuloa->deskribapena;?></p>
-    <small>Egilea: <?php echo $artikuloa->idazlea;?></small>
-  </a>
-</div>	
+      <div class="list-group" onclick="console.log('patata');">
+        <a href="<?php echo $artikuloa->textua;?>" class="list-group-item list-group-item-action flex-column align-items-start">
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1"><?php echo $artikuloa->izenburua;?></h5>
+            <small><?php echo $artikuloa->saila; ?></small>
+         </div>
+          <p class="mb-1"><?php echo $artikuloa->deskribapena;?></p>
+          <small>Egilea: <?php echo $artikuloa->idazlea;?></small>
+       </a>
+      </div>	
 
-  <?php
+<?php
+    }
 }
-
 ?>
