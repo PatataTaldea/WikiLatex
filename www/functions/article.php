3e9e8a62b7f9ed4b9edb33f6e101b6a776bbc12f
osainz59 <osainz59@gmail.com>
2017-12-08 14:02:04 +0100

    Gomendioak

--- www/functions/article.php
+++ www/functions/article.php
@@ -3,6 +3,10 @@ require '../config.php';
 $data = simplexml_load_file("../".ARTIKULUAK) or die("Error: Cannot create object");
 
 
+// Nondik atera duzue $artikuloa aldagaia?
+// Nahi duzuen artikuloa bilatzeko XML-an xpath erabili, horrek array bat itzuliko dizue, [0] posizioan egongo da nahi duzuena baldin badago
+// Artikuloaren barneko textua lorzeko xml_functions.php-en definitu dut funtzio bat $artikuloa aldagaia emanda html osoa string batean itzultzen duena.
+// Dena echo bezela printeatu beharrean pixkat txukunago egitea komeni litzake ezta? Adibidez izenburua <h1> etiketen artean jartzea, etab... 
 echo $artikuloa->izenburua;
 
 echo $artikuloa->deskribapena;
