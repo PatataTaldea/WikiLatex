1b7e129c7a600f7522ce9c084cd2355d5d200d9e
Egonichan <egoninobi@gmail.com>
2017-12-08 20:48:00 +0100

    lortu_artikuloarekin errorea dago

--- www/functions/article.php
+++ www/functions/article.php
@@ -1,5 +1,6 @@
 <?php
 require '../config.php';
+require '../functions/xml_functions.php';
 $data = simplexml_load_file("../".ARTIKULUAK) or die("Error: Cannot create object");
 $artikuloa = $data->xpath("/artikuloak/artikuloa[izenburua='".$_GET['izenburua']."']")[0];
 
@@ -12,7 +13,7 @@ echo $artikuloa->deskribapena;
 
 echo $artikuloa->saila;
 
-echo lortu_artikuloa($artikuloa->idazlea_textua);
+$html = lortu_artikuloa($artikuloa);
 
 echo $artikuloa->idazlea_izena;
 echo $artikuloa->idazlea;
