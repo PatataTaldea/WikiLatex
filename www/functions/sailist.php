80fd077d659f99602a18e32eb3d260f9b440cb6d
Egonichan <egoninobi@gmail.com>
2017-12-08 12:41:40 +0100

    iruzkin

--- www/functions/sailist.php
+++ www/functions/sailist.php
@@ -61,6 +61,36 @@ $data = simplexml_load_file("../".ARTIKULUAK) or die("Error: Cannot create objec
     </div>
   </div>
 <?php 
+} else if (isset($_GET['saila']) && $_GET['saila']=='Galderak') {
+  ?>
+    <h1>Esaiguzu zerbait<button type="button" class="btn btn-outline-primary" style="float: right;" onclick="iruzkindu()">Iruzkindu</button></h1>
+    <div><table class="table">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Izena</th>
+      <th scope="col">Iruzkina</th>
+	  <th scope="col">Data</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+    $kont = 0;
+    foreach($data->artikuloa as $artikuloa){
+  ?>
+    <tr>
+      <td><?php echo $kont+1;?></td>
+      <td>Doraemon</td>
+      <td>ありがとうございます</td>
+	    <td>2015/09/03</td>
+    </tr>
+<?php
+    $kont = $kont+1;
+    }
+    ?>
+  </tbody>
+</table></div>
+<?php	
 } else if (isset($_GET['saila']) && $_GET['saila']=='Nondik hasi') {
   ?>
     <h1>Nola hasi Latex erabiltzen<button type="button" class="btn btn-outline-primary" style="float: right;" onclick="sortuArtikulua()">Sortu artikulu berri bat</button></h1>
@@ -84,7 +114,7 @@ $data = simplexml_load_file("../".ARTIKULUAK) or die("Error: Cannot create objec
     <h1>Ekuazioak, funtzioak, ikurrak eta abar<button type="button" class="btn btn-outline-primary" style="float: right;" onclick="sortuArtikulua()">Sortu artikulu berri bat</button></h1>
 <?php
     foreach($data->artikuloa as $artikuloa){
-      if ($artikuloa->saila!="Nondik hasi" && $artikuloa->saila!="Galderak"){
+      if ($artikuloa->saila!="Nondik hasi"){
   ?>
       <div class="list-group-item list-group-item-action flex-column align-items-start" onclick="idatziArtikulua('<?php echo $artikuloa->textua;?>');">
           <div class="d-flex w-100 justify-content-between">
@@ -95,24 +125,6 @@ $data = simplexml_load_file("../".ARTIKULUAK) or die("Error: Cannot create objec
           <small>Egilea: <?php echo $artikuloa->idazlea;?></small>
       </div>	
 <?php
-      }
-    }
-} else if (isset($_GET['saila']) && $_GET['saila']=='Galderak') {
-  ?>
-    <h1>Latexi buruzko galdera sortak<button type="button" class="btn btn-outline-primary" style="float: right;" onclick="sortuArtikulua()">Sortu artikulu berri bat</button></h1>
-<?php
-    foreach($data->artikuloa as $artikuloa){
-      if ($artikuloa->saila=="Galderak"){
-  ?>
-      <div class="list-group-item list-group-item-action flex-column align-items-start" onclick="idatziArtikulua('<?php echo $artikuloa->textua;?>');">
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1"><?php echo $artikuloa->izenburua;?></h5>
-            <small><?php echo $artikuloa->saila; ?></small>
-         </div>
-          <p class="mb-1"><?php echo $artikuloa->deskribapena;?></p>
-          <small>Egilea: <?php echo $artikuloa->idazlea;?></small>
-      </div>
-<?php	
       }
     }
 }
