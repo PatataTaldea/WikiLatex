dfcd8af9d56f15b7c5097cb18670855a83268b27
Egonichan <egoninobi@gmail.com>
2017-12-09 20:55:57 +0100

    artikuluen plantilla egiten

--- www/functions/article.php
+++ www/functions/article.php
@@ -17,17 +17,21 @@ if (isset($_GET['artikuloa'])){
 }
 
 if (isset($artikuloa)) {
-
-    echo $artikuloa->izenburua;
-    
-    echo $artikuloa->deskribapena;
-    
-    echo $artikuloa->saila;
-    
-    $html = lortu_artikuloa($artikuloa);
-    
-    echo $artikuloa->idazlea_izena;
-    echo $artikuloa->idazlea;
+?>
+    <div class="card text-center">
+        <div class="card-header text-muted">
+            <p align="left"><?php echo $artikuloa->saila;?> -> <?php echo $artikuloa->deskribapena;?></p>
+        </div>
+        <div class="card-body">
+            <h1 class="card-title"><u><?php echo $artikuloa->izenburua;?></u></h1>
+            <p class="card-text"><?php $html = lortu_artikuloa($artikuloa);?></p>
+        </div>
+        <div class="card-footer text-muted">
+            <h3 align="right"><?php echo $artikuloa->idazlea_izena;?></h3>
+            <p align="right"><?php echo $artikuloa->idazlea;?></p>
+        </div>
+    </div>
+<?php
 }
 
 ?>
