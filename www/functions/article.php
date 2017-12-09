5148af6aba0770d111e0e7e473b470c7c95d979d
Egonichan <egoninobi@gmail.com>
2017-12-09 22:15:19 +0100

    artikuluak txukuntzen

--- www/functions/article.php
+++ www/functions/article.php
@@ -25,7 +25,7 @@ if (isset($artikuloa)) {
         </div>
         <div class="card-body">
             <h1 class="card-title"><u><?php echo $artikuloa->izenburua;?></u></h1>
-            <p class="card-text"><?php $html = lortu_artikuloa($artikuloa,$ROOTPATH);?></p>
+            <p class="card-text"><?php echo lortu_artikuloa($artikuloa,$ROOTPATH);?></p>
         </div>
         <div class="card-footer text-muted">
             <h3 align="right"><?php echo $artikuloa->idazlea_izena;?></h3>
