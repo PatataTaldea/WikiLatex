bbcf439e8833934300b3ed84fb236e18540e5ca9
Oscar Sainz Jimenez <osainz59@gmail.com>
2017-12-10 21:15:01 +0100

    Artikuloen eta sailen responsive egitura ezarri Boostrapen bidez

--- www/pages/artikuloa.php
+++ www/pages/artikuloa.php
@@ -1,13 +1,20 @@
-<div class="card text-center">
-    <div class="card-header text-muted">
-        <p align="left"><?php echo $artikuloa->saila;?> -> <?php echo $artikuloa->deskribapena;?></p>
+<div class="row">
+    <div class="col-11">
+        <div class="card">
+            <div class="card-header text-muted">
+                <p align="left"><?php echo $artikuloa->saila;?> -> <?php echo $artikuloa->deskribapena;?></p>
+            </div>
+            <div class="card-body">
+                <h1 class="card-title"><u><?php echo $artikuloa->izenburua;?></u></h1>
+                <p class="card-text"><?php echo lortu_artikuloa($artikuloa,$ROOTPATH);?></p>
+            </div>
+            <div class="card-footer text-muted">
+                <h3 align="right"><?php echo $artikuloa->idazlea_izena;?></h3>
+                <p align="right"><?php echo $artikuloa->idazlea;?></p>
+            </div>
+        </div>
     </div>
-    <div class="card-body">
-        <h1 class="card-title"><u><?php echo $artikuloa->izenburua;?></u></h1>
-        <p class="card-text"><?php echo lortu_artikuloa($artikuloa,$ROOTPATH);?></p>
-    </div>
-    <div class="card-footer text-muted">
-        <h3 align="right"><?php echo $artikuloa->idazlea_izena;?></h3>
-        <p align="right"><?php echo $artikuloa->idazlea;?></p>
+    <div class="col-1">
     </div>
 </div>
+
--- www/pages/saila.php
+++ www/pages/saila.php
@@ -1,18 +1,25 @@
 <?php
-$data = simplexml_load_file("../".ARTIKULUAK) or die("Error: Cannot create object");
-  $artikuloak = $data->xpath("/artikuloak/artikuloa[saila='".$_GET['saila']."']");
-  ?>
-    <h1><?php echo $_GET['saila'];?></h1>
-<?php
-    foreach($artikuloak as $artikuloa){
-  ?>
-      <div class="list-group-item list-group-item-action flex-column align-items-start" onclick="idatziArtikulua('<?php echo $artikuloa->izenburua;?>','<?php echo $artikuloa->saila;?>');">
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1"><?php echo $artikuloa->izenburua;?></h5>
-            <small><?php echo $artikuloa->saila; ?></small>
-         </div>
-          <p class="mb-1"><?php echo $artikuloa->deskribapena;?></p>
-          <small>Egilea: <?php echo $artikuloa->idazlea;?></small>
-      </div>	
-<?php
-    }
+    $data = simplexml_load_file("../".ARTIKULUAK) or die("Error: Cannot create object");
+    $artikuloak = $data->xpath("/artikuloak/artikuloa[saila='".$_GET['saila']."']");
+?>
+<div class="row">
+    <div class="col-11">
+        <h1><?php echo $_GET['saila'];?></h1>
+        <?php
+        foreach($artikuloak as $artikuloa){
+        ?>
+            <div class="list-group-item list-group-item-action flex-column align-items-start" onclick="idatziArtikulua('<?php echo $artikuloa->izenburua;?>','<?php echo $artikuloa->saila;?>');">
+                <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1"><?php echo $artikuloa->izenburua;?></h5>
+                    <small><?php echo $artikuloa->saila; ?></small>
+                </div>
+                <p class="mb-1"><?php echo $artikuloa->deskribapena;?></p>
+                <small>Egilea: <?php echo $artikuloa->idazlea;?></small>
+            </div>	
+        <?php
+        }
+        ?>
+    </div>
+    <div class="col-1">
+    </div>
+</div>
