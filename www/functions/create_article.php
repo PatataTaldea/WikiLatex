78d4a4d21cf4031313e603de0ffc46382b6f35f2
Maite <urragorospe@hotmail.com>
2017-12-11 18:26:12 +0100

    Kode iruzkindua

--- www/functions/create_article.php
+++ www/functions/create_article.php
@@ -8,44 +8,36 @@ require 'xml_functions.php';
 
 if (isset($_POST['editor']) ){
 
-
-
+	//Fitxategiak kargatu
 	$artikuluak=simplexml_load_file('../'.EZ_ARTIKULUAK) or die("Error: Cannot create object");
 	$artikuluak2=simplexml_load_file('../'.ARTIKULUAK) or die("Error: Cannot create object");
 	$art = $artikuluak->xpath("artikuloa[izenburua='".$_POST['izenburua']."']")[0];
 	$art2 = $artikuluak2->xpath("artikuloa[izenburua='".$_POST['izenburua']."']")[0];
 
-    
+    //Izenburu bereko fitxategirik ez dagoela zihurtatu
     if($art == NULL && $art2 == NULL) {
 
+    	//Artikulu berria sortu 
 		$berria=$artikuluak->addChild('artikuloa');
-
-	
-		
 		$hitzGakoak=$berria->addChild('hitzgakoak');
-
 		$hitzGakoa=explode(",",$_POST['hitzGakoak']);
 		
 		foreach ($hitzGakoa as $gako){
 			$hitzGakoak->addChild('hitza',$gako);
 		}
 
-
 		$berria->addChild('izenburua',$_POST['izenburua']);
 		$berria->addChild('deskribapena',$_POST['deskribapena']);
-
 		$berria->addChild('saila',$_POST['saila']);
 
 		if(isset($_SESSION['erabiltzailea'])){
 			$berria->addChild('idazlea_izena',substr($_SESSION['erabiltzailea'],0));
 		}
 		
-		
 		if(isset($_SESSION['erab_email'])){
 			$berria->addChild('idazlea',substr($_SESSION['erab_email'],0));
 		}
 		
-
 		$contenido = $_GET['editor'];
 
 		$sail = preg_replace('/\s+/', '_', $_POST['saila']);
@@ -55,18 +47,16 @@ if (isset($_POST['editor']) ){
 
 		$berria->addChild('textua',$sail.'/'.$_POST['izenburua'].'.html');
 		
+
+		//XML fitxategian gorde
 	    save_formated($artikuluak, "../".EZ_ARTIKULUAK);
 
 	    echo '0';
-
 
 	}   
 	else{
 		echo '2';
 	} 
-
        
 } else echo '1';
-
-
 ?>
